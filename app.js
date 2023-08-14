bc5730f834fc38724c53bce8968203004d0f2e96
HotG8119 <k508060@gmail.com>
2023-08-14 20:18:38 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const methodOverride = require('method-override')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -38,6 +38,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
---
+++ controllers/apis/restaurant-controller.js
@@ -0,0 +1,47 @@
+const { Restaurant, Category } = require('../../models')
+const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+
+const restaurantController = {
+  getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT = 9
+    const categoryId = Number(req.query.categoryId) || ''
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        raw: true,
+        nest: true,
+        include: [Category],
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        limit,
+        offset
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user?.FavoritedRestaurants ? req.user.FavoritedRestaurants.map(fr => fr.id) : []
+        const likedRestaurantsId = req.user?.LikedRestaurants ? req.user.LikedRestaurants.map(lr => lr.id) : []
+        const data = restaurants.rows.map(restaurant =>
+          ({
+            ...restaurant,
+            description: restaurant.description.substring(0, 50),
+            isFavorited: favoritedRestaurantsId.includes(restaurant.id),
+            isLiked: likedRestaurantsId.includes(restaurant.id)
+          }))
+        return res.json({
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
+      })
+      .catch(error => next(error))
+  }
+}
+
+module.exports = restaurantController
