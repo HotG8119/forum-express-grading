fc0f5f6df754ce2d61613104b51801454f072ead
HotG8119 <k508060@gmail.com>
2023-08-14 23:24:13 +0800

    feat: add restaurant-services

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,46 +1,10 @@
 const { Restaurant, Category, User, Comment } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+
+const restaurantServices = require('../../services/restaurant-services')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9
-    const categoryId = Number(req.query.categoryId) || ''
-
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        raw: true,
-        nest: true,
-        include: [Category],
-        where: {
-          ...categoryId ? { categoryId } : {}
-        },
-        limit,
-        offset
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
-        const likedRestaurantsId = req.user && req.user.LikedRestaurants.map(lr => lr.id)
-        const data = restaurants.rows.map(restaurant =>
-          ({
-            ...restaurant,
-            description: restaurant.description.substring(0, 50),
-            isFavorited: favoritedRestaurantsId.includes(restaurant.id),
-            isLiked: likedRestaurantsId.includes(restaurant.id)
-          }))
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(error => next(error))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render(data))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
